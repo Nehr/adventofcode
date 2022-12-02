2e6e68b31adab47a0ece2814b030f8c7acbb6a82
Thom <thom@websquid.se>
2022-12-02 17:20:03 +0100

    Day two

--- 2022/2/main.py
+++ 2022/2/main.py
@@ -3,18 +3,14 @@ import re
 def get_data() -> list:
     print(f"{get_data.__name__}()")
     temp_data = []
-    with open('test_data.txt', 'r', encoding='utf-8') as the_file:
+    with open('data.txt', 'r', encoding='utf-8') as the_file:
         data = the_file.read().splitlines()
         for line in data:
             temp_data.append(line)
     return temp_data
 
 
-def part_two(_data) -> None:
-    print(f"{part_two.__name__}():")
-
-
-def get_item_points(item):
+def get_item_points(item) -> int:
     match item:
         case "rock":
             return 1
@@ -25,17 +21,17 @@ def get_item_points(item):
         case _:
             return 0
 
-def get_round_points(outcome):
+def get_round_points(outcome) -> int:
     match outcome:
-        case "win":
+        case 1:
             return 6
-        case "draw":
+        case 0:
             return 3
         case _:
             return 0
 
 
-def calc_win(you, opp):
+def calc_win(you, opp) -> int:
     match you:
         case "rock":
             if opp == "paper":
@@ -54,38 +50,79 @@ def calc_win(you, opp):
                 return 1
     return 0
 
+def get_loss_item(opp) -> str:
+    match opp:
+        case "rock":
+            return "scissors"
+        case "scissors":
+            return "paper"
+        case _:
+            return "rock"
 
-# X for Rock, Y for Paper, and Z for Scissors
-# A for Rock, B for Paper, and C for Scissors
-def type_converter(letter):
-    print(f"{letter=}")
-    match letter:
-        case ['X', 'A']:
-            return 'rock'
-        case ['Y', 'B']:
-            return 'paper'
-        case ['Z', 'C']:
-            return 'scissors'
+def get_win_item(opp) -> str:
+    match opp:
+        case "rock":
+            return "paper"
+        case "scissors":
+            return "rock"
+        case _:
+            return "scissors"
 
+
+def item_converter(letter) -> str:
+    if letter in ('X', 'A'):
+        return 'rock'
+    if letter in ('Y', 'B'):
+        return 'paper'
+    if letter in ('Z', 'C'):
+        return 'scissors'
+    return ''
+
+
+def part_two_conclusion(conclusion, opp) -> int:
+    if conclusion == 'X':
+        # loss
+        your_item = get_loss_item(opp)
+        return get_round_points(-1) + get_item_points(your_item)
+    if conclusion == 'Z':
+        # win
+        your_item = get_win_item(opp)
+        return get_round_points(1) + get_item_points(your_item)
+    # draw
+    your_item = opp
+    return get_round_points(0) + get_item_points(your_item)
 
 def part_one(data) -> None:
-    print(f"{part_one.__name__}():")
-    you_regex = re.compile(r'A|B|C')
-    opp_regex = re.compile(r'X|Y|Z')
+    print(f"\n{part_one.__name__}()\n---------")
+    reg_data = re.compile(r'(\w) (\w)')
+    total = 0
     for line in data:
-        print(line)
-        you_val = you_regex.search(line).group()
-        print(f"{you_val=}")
-        print(you_val == 'A')
-        you_item = type_converter(you_val)
-        print(f"{you_item=}")
-        #print(you_val.group())
-        opp_val = opp_regex.search(line).group()
-        opp_item = type_converter(opp_val)
-        print(f"{opp_item=}")
-        #print(opp_val.group())
+        reg_result = reg_data.search(line)
+        opp_regex = reg_result.group(1)
+        you_regex = reg_result.group(2)
+        you_item = item_converter(you_regex)
+        opp_item = item_converter(opp_regex)
         is_win = calc_win(you_item, opp_item)
-        print(f"{is_win=}")
+        round_points = get_round_points(is_win)
+        item_points = get_item_points(you_item)
+        round_total = round_points + item_points
+        total += round_total
+    print(f'{total=}')
+
+
+def part_two(data) -> None:
+    print(f"\n{part_two.__name__}()\n---------")
+    reg_data = re.compile(r'(\w) (\w)')
+    total = 0
+    for line in data:
+        reg_result = reg_data.search(line)
+        opp_regex = reg_result.group(1)
+        opp_item = item_converter(opp_regex)
+        round_result = reg_result.group(2)
+        total += part_two_conclusion(round_result, opp_item)
+    print(f'{total=}')
+
+
 
 
 def main() -> None:
@@ -93,6 +130,7 @@ def main() -> None:
     #print(data)
     part_one(data)
     part_two(data)
+    print("\n------\nexit()")
 
 
 if __name__ == "__main__":
