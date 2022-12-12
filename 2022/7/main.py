2bd3ddcc6ff77541e61004a879026e74b659b3d1
Thom <thom@websquid.se>
2022-12-12 17:29:07 +0100

    Day 7, part 1

--- 2022/7/main.py
+++ 2022/7/main.py
@@ -19,7 +19,9 @@ def get_data(is_test: bool) -> list:
 
 
 def get_action(line: str) -> dict | None:
+    #print(f'{line=}')
     is_action = actions.findall(line)
+    #print(f'{is_action=}')
     if len(is_action) > 0:
         action = list(is_action[0])
         param = None
@@ -105,63 +107,82 @@ def count_total_in_dirs(filesystem: dict, max_size: int) -> int:
         print(f'{str(dir_total)} = {dir_total <= max_size}')"""
     for direct in filesystem:
         dir_total = get_total_from_dir_recursive(direct, filesystem)
-        if dir_total <= max_size:
+        if max_size > 0 and dir_total <= max_size:
+            total += dir_total
+        elif max_size == 0:
             total += dir_total
         #print(f'{str(dir_total)} = {dir_total <= max_size}')
     return total
 
 
-def part_one(data: list) -> None:
-    print(f"\n{part_one.__name__}()\n---------")
+def part_one(data: list, count_max: int, print_data: bool) -> dict:
+    if print_data:
+        print(f"\n{part_one.__name__}()\n---------")
     filesystem = {}
     current_dir = []
-    #add_files = False
     for line in data:
-        #print(f'{current_dir=}')
         action = get_action(line)
+        if print_data:
+            print(f'{action=}')
         if action is not None:
-            #add_files = False
-            #print(f'{action=}')
             if action["action"] == 'cd':
                 if action["param"] == '/':
                     filesystem[action["param"]] = mkdir(action["param"], None)
                     current_dir = ['/']
                 elif action["param"] != '..':
-                    if action["param"] in filesystem:
-                        print(f'Error? {filesystem[action["param"]]}')
-                        # folders can have same name, group by dir path?
-                    filesystem[action["param"]] = mkdir(action["param"], current_dir[-1])
+                    full_dir_name = ''.join(current_dir)
+                    #parent_dir_name = ''.join(current_dir[:-1])
+                    new_dir_name = full_dir_name + action["param"]
+                    filesystem[new_dir_name] = mkdir(action["param"], current_dir)
                     current_dir.append(action["param"])
                 else:
                     current_dir.pop()
-            #if action["action"] == 'ls':
-            #    add_files = True
         else:
             is_file_or_dir = get_file_or_dir(line)
+            full_dir_name = ''.join(current_dir)
             if is_file_or_dir['type'] == 'dir':
-                filesystem[current_dir[-1]]['children'].append(is_file_or_dir['name'])
+                new_dir_name = full_dir_name + is_file_or_dir['name']
+                filesystem[full_dir_name]['children'].append(new_dir_name)
             else:
                 the_file = touch(is_file_or_dir['name'], is_file_or_dir['size'])
-                filesystem[current_dir[-1]]['files'].append(the_file)
-                filesystem[current_dir[-1]]['size'] += int(the_file['size'])
-            #print(f'adding file/dir: {is_file_or_dir=}')
-    #total = get_total_from_dir_recursive('/', filesystem)
-    #print(json.dumps(filesystem, sort_keys=True, indent=4) + '\n')
-    #test = get_parent_total_recursive('e', filesystem)
-    #print(f'{test=}')
-    total = count_total_in_dirs(filesystem, 100000)
-    print(f'\n{total=}')
+                filesystem[full_dir_name]['files'].append(the_file)
+                filesystem[full_dir_name]['size'] += int(the_file['size'])
+    if print_data:
+        print(json.dumps(filesystem, sort_keys=True, indent=4) + '\n')
+    total = count_total_in_dirs(filesystem, count_max)
+    if print_data:
+        print(f'\n{total=}')
+    return filesystem
 
 
-def part_two(_data: list) -> None:
+def part_two(data: list) -> None:
     print(f"\n{part_two.__name__}()\n---------")
+    disk_size = 70000000
+    unused_space_needed = 30000000
+    max_used_disk_size = disk_size - unused_space_needed
+    filesystem = part_one(data, 0, False)
+    print(f'{max_used_disk_size=}')
+    items = filesystem.items()
+    root_size = 0
+    for item in items:
+        print(item[1]['name'], item[1]['parent'])
+        if item[1]['parent'] == '/':
+            print("item has parent /")
+            root_size += item[1]['size']
+    print(f'{root_size=}')
+    print('/ size: ', filesystem["/"]["size"])
+    for item in items:
+        item_size = item[1]['size']
+        print(f'{item_size=}')
+        print('disk_size - item_size: ', disk_size - item_size)
+        print('disk_size - item_size < max_used_disk_size: ', disk_size - item_size < max_used_disk_size)
     #for line in data:
     #    print(line)
 
 
 def main() -> None:
-    data = get_data(True)
-    part_one(data)
+    data = get_data(False)
+    part_one(data, 100000, True)
     part_two(data)
     print("\n------\nexit()")
 
