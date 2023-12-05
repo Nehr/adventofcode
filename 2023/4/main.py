ddd89816e715938777ad075c9f8ce8216a249d1b
Thom <thom@websquid.se>
2023-12-05 17:41:59 +0100

    Day 4, part 2

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -36,13 +36,13 @@ def get_data(file_type: FileType) -> list:
 
 
 def get_card_data(line: str) -> dict or None:
-    logging.debug("%s()", get_card_data.__name__)
+    #logging.debug("%s()", get_card_data.__name__)
     match = re.search(r'Card\s*(\d+):\s*((\d+\s*)*)\|', line)
     if match:
-        logging.debug("card: %s", match.group(1))
+        #logging.debug("card: %s", match.group(1))
         numbers = re.findall(r'\d+', match.group(2))
         winning_numbers = [int(num) for num in numbers]
-        logging.debug("winning numbers: %s", winning_numbers)
+        #logging.debug("winning numbers: %s", winning_numbers)
         return {
             'card': int(match.group(1)),
             'winning_numbers': winning_numbers
@@ -51,12 +51,12 @@ def get_card_data(line: str) -> dict or None:
 
 
 def get_user_data(line: str) -> list or None:
-    logging.debug("%s()", get_user_data.__name__)
+    #logging.debug("%s()", get_user_data.__name__)
     match = re.search(r'\|\s*((\d+)\s*)*', line)
     if match:
         numbers = re.findall(r'\d+', match.group(0))
         user_data = [int(num) for num in numbers]
-        logging.debug("user data: %s", user_data)
+        #logging.debug("user data: %s", user_data)
         return user_data
     return None
 
@@ -92,10 +92,28 @@ def part_one(data: list) -> None:
     logging.debug("end %s\n", part_one.__name__)
 
 
+def add_winnings(card_amounts: dict, card: int, common_numbers_count: int) -> None:
+    logging.debug("%s(%s - %s)", add_winnings.__name__, card + 1, card + common_numbers_count)
+    total_wins = 0
+    for i in range(card + 1, card + common_numbers_count + 1):
+        logging.debug("card winnings: %s", i)
+        if i not in card_amounts:
+            card_amounts[i] = 2
+        else:
+            card_amounts[i] += 1
+        total_wins += 1
+    logging.debug("total wins from card %s = %s", card, total_wins)
+    return total_wins
+
+
 def part_two(data: list) -> None:
     logging.info("%s()", part_two.__name__)
+    total = 0
+    cards_count = 0
+    card_amounts = {}
     for line in data:
         logging.debug("%s", line)
+        cards_count += 1
         card = get_card_data(line)
         if card is None:
             logging.error("card is None")
@@ -103,13 +121,32 @@ def part_two(data: list) -> None:
         card_array = card['winning_numbers']
         user_data = get_user_data(line)
         common_numbers_count = count_common_numbers(card_array, user_data)
+        current_card = card['card']
+
+        if current_card not in card_amounts:
+            card_amounts[current_card] = 1
         logging.debug("common numbers count: %s", common_numbers_count)
+
+        logging.debug("card scratches: %s", card_amounts[current_card])
+
+        if common_numbers_count != 0:
+            for i in range(card_amounts[current_card]):
+                logging.debug("card scratch %s / %s", i + 1, card_amounts[current_card])
+                winnings = add_winnings(card_amounts, current_card, common_numbers_count)
+                logging.debug("winnings: %s", winnings)
+                total += winnings
+                logging.debug("")
+        else:
+            logging.debug("no card scratches")
+        logging.debug("")
+    logging.debug("card_amounts: %s", card_amounts)
+    logging.info("total: %s", total + cards_count)
     logging.debug("end %s\n", part_two.__name__)
 
 
 def main() -> None:
-    setup_logger(logging.DEBUG)
-    file_type = FileType.TEST
+    setup_logger(logging.INFO)
+    file_type = FileType.REAL
     data = get_data(file_type)
     #part_one(data)
     part_two(data)
