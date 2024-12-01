253f56e83741839c61f74920eac829b738dde9d4
Thom <thom@websquid.se>
2024-12-01 13:22:34 +0100

    Day 1 2024

--- 2024/1/main.py
+++ 2024/1/main.py
@@ -3,6 +3,7 @@
 import enum
 import logging
 import argparse
+import re
 
 
 class FileType(enum.Enum):
@@ -34,18 +35,46 @@ def get_data(file_type: FileType) -> list:
         logging.error("File not found: %s", file_name)
         return []
 
+def get_lists(data: list) -> tuple:
+    re_get_digits: str = re.compile(r'(\d+)\s+(\d+)')
+    left_list: list = []
+    right_list: list = []
+
+    for line in data:
+        matches = re_get_digits.match(line)
+        if matches:
+            left_list.append(int(matches.group(1)))
+            right_list.append(int(matches.group(2)))
+            logging.debug("left: %s, right: %s", matches.group(1), matches.group(2))
+
+    return left_list, right_list
 
 def part_one(data: list) -> None:
     logging.info("%s()", part_one.__name__)
-    for line in data:
-        logging.debug(line)
+    total: int = 0
+    left_list, right_list = get_lists(data)
+
+    left_list = sorted(left_list)
+    right_list = sorted(right_list)
+
+    for i, _ in enumerate(left_list):
+        difference = left_list[i] - right_list[i]
+        total += abs(difference)
+        logging.debug("%d - %d: %d, total: %d", left_list[i], right_list[i], abs(difference), total)
+
+    logging.info("Total: %d", total)
     logging.debug("end %s\n", part_one.__name__)
 
 
 def part_two(data: list) -> None:
     logging.info("%s()", part_two.__name__)
-    for line in data:
-        logging.debug("%s", line)
+    total: int = 0
+    left_list, right_list = get_lists(data)
+
+    for i, _ in enumerate(left_list):
+        total += left_list[i] * right_list.count(left_list[i])
+
+    logging.info("Total: %d", total)
     logging.debug("end %s\n", part_two.__name__)
 
 
@@ -62,7 +91,7 @@ def main() -> None:
         file_type = FileType.TEST
     data = get_data(file_type)
     part_one(data)
-    #part_two(data)
+    part_two(data)
     logging.debug("end %s()", main.__name__)
 
 
